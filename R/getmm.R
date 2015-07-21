3efa87d2faa7444a6a02ea45d257c72d5d46f50b
jjchern <jiajia.chern@gmail.com>
2015-07-20 23:29:12 -0500

    getmm: Return a tbl_df.

--- R/getmm.R
+++ R/getmm.R
@@ -2,14 +2,15 @@
 #'
 #' @param data A string specifying the name of a Mastering 'Metrics dataset.
 #' @param envir An environment in which to store the object. Default is the parent environment.
-#' @return A dataset.
+#' @return A dataframe.
 #' @examples
 #' getmm("mlda")
-#' @import rio
+#' @import rio dplyr
 #' @export
 getmm = function(data, envir = parent.frame()) {
   if(data == "mlda"){
-    dat = rio::import("http://masteringmetrics.com/wp-content/uploads/2015/01/AEJfigs.dta")
+    dat = rio::import("http://masteringmetrics.com/wp-content/uploads/2015/01/AEJfigs.dta") %>%
+      dplyr::tbl_df()
     assign(data, dat, envir = envir)
     invisible(dat)
   }
